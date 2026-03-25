fa7b3c098a41e65ee3c471fe6c6987ef7dcb02c6
mkrisna1 <mkrisnanugraha@gmail.com>
2026-03-25 16:15:51 +0700

    sementara

--- src/pages/putra.js
+++ src/pages/putra.js
@@ -1,9 +1,38 @@
-function putra() {
+import ProfileLayout from "../layout/layout";
+import putraPhoto from "../images/Putra.png";
+
+export default function putra() {
+
+
   return (
-    <div>
-      <h1>Halaman putra</h1>
-    </div>
+    <ProfileLayout
+      name="Ahmad Bintang Putra Fajariyanto"
+      studentClass="T2B"
+      major="Teknologi Informasi"
+      domicile="Pasuruan"
+      photo={putraPhoto}
+      about={
+        <>
+          <p>
+            Saya adalah mahasiswa Universitas Brawijaya dengan minat yang kuat dalam dunia teknologi,
+            Khususnya pengembangan web dan desain antarmuka. Sejak awal kuliah, saya antusias
+            Mengeksplorasi proses pemuatan aplikasi dari desain ui hingga logika di baliknya.
+            Ketertarikan ini mendorong saya untuk mendalami ReactJS, memahami komponen secara mendetail
+            serta menguasi manejemen state untuk membangun aplikasi yang efisien dan 
+            interaktif
+          </p>
+          <p>
+            Biodata
+            Nama : Ahmad bintang putra fajariyanto
+            Universitas : Universitas Brawijaya 
+            Program studi : Teknologi Inforamasi
+            Minat : Pengembangan web, Teknologi interaktif 
+            Deskripsi singkat : 
+            Mahasiwa Universitas brawijaya dengan minat kuat dalam belajar tentang teknologi
+            Antusias membangun aplikasi yang efisien dan interaktif. 
+          </p>
+        </>
+      }
+    />
   );
 }
-
-export default putra;
